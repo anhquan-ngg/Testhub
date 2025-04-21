419afdef687a0e379fbf5dfa13f3b904423eee8c
Nguyen Anh Quan <nanhquan143@gmail.com>
2025-04-21 13:46:34 +0700

    init auth page

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Auth from './pages/auth';
+import Profile from './pages/profile';
+import Exam from './pages/exam';
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth/>}/>
+        <Route path="/auth/profile" element={<Profile/>} />
+        <Route path="/exams" element={<Exam/>} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ client/src/pages/auth/index.jsx
@@ -0,0 +1,13 @@
+import '@/index.css';
+
+const Auth = () => {
+  return (
+    <div className="h-screen w-screen bg-[#b8cae8] flex items-center justify-center" >
+      <div className='h-135 w-126 bg-white rounded-lg'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Auth
